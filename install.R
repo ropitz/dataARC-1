c62adb240d27929c76b7c65bd01dd4bf855077d8
andresgmejiar <46786423+andresgmejiar@users.noreply.github.com>
2021-03-04 14:23:20 -0500

    Add files via upload

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("R.utils","rgdal","raster","rbioclim","stringr","stringi",                   "proj4","data.table","FSA","plyr","wordcloud","mapview","KernSmooth","gdtools","FactoMineR","factoextra","sp","plyr","ggplot2","plotly","rgeos","randomForest","devtools","gdata","jsonlite")
+list.of.packages <- c("R.utils","sf","rgdal","raster","rbioclim","stringr","stringi","proj4","data.table","FSA","plyr","wordcloud","mapview","KernSmooth","gdtools","FactoMineR","factoextra","sp","plyr","ggplot2","plotly","rgeos","randomForest","devtools","gdata","jsonlite")
 
 install.packages(list.of.packages)
 
