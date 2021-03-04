95170c08df7c65dd64887e1ed467fc745b08cfce
andresgmejiar <46786423+andresgmejiar@users.noreply.github.com>
2021-03-04 16:25:39 -0500

    Add files via upload

--- install.R
+++ install.R
@@ -1,7 +1,18 @@
-list.of.packages <- c("R.utils","sf","rgdal","raster","rbioclim","stringr","stringi","proj4","data.table","FSA","plyr","wordcloud","mapview","KernSmooth","gdtools","FactoMineR","factoextra","sp","plyr","ggplot2","plotly","rgeos","randomForest","devtools","gdata","jsonlite")
+list.of.packages <- c("R.utils","sf","rgdal","raster","rbioclim","stringr","stringi","proj4","data.table","FSA","plyr","wordcloud","mapview","KernSmooth","gdtools","FactoMineR","factoextra","sp","plyr","ggplot2","plotly","rgeos","randomForest","devtools","gdata","jsonlite","knitr","tibble","dplyr","tidyr","purrr")
 
 install.packages(list.of.packages)
 
 gdata::installXLSXsupport()
 
 devtools::install_github("MoisesExpositoAlonso/rbioclim")
+
+
+library(gdata) # for the trim function
+library(jsonlite)
+library(knitr)
+library(stringr)
+library(tibble)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(data.table)
